f45af915daf2a625196bbedee6715bb4291c35a9
lujun <lujun@ali-6c96cfd98a9b.local>
2018-03-19 17:39:07 +0800

    “”

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import "./style.less";
 
 export default class App extends React.Component{
 	render(){
-		return <div> Hello main.js </div>;
+		return (
+			<div className="page">
+				<h1 className="h1">Hello main.js</h1>
+			</div>
+		)
 	}
 }
 
